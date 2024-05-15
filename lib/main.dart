55d6fc72de58b5b2194c615385579946bbc45c2e
mimo626 <yessuk5499@gmail.com>
2024-05-15 18:39:09 +0900

    [feat]Container에 BoxDecoration 구현

--- lib/main.dart
+++ lib/main.dart
@@ -4,40 +4,43 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text("App bar"),
-        centerTitle: false,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.home),
-            onPressed: () {
-              print("Tab");
-            },
-          ),
-          Icon(Icons.add),
-        ],
+        title: Text("Study to Container"),
       ),
-      body: TestWidget(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.bug_report),
-        onPressed: (){
-          print("Tab! Fab!");
-        },
-      ),
+      body: CustomContainer(),
     ),
   ));
 }
 
-class TestWidget extends StatelessWidget {
-  const TestWidget({super.key});
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: Text(
-          "Hello Flutter",
-          style: TextStyle(fontSize: 60, color: Colors.black),
+    return Center(
+      child: Container(
+        width: 300,
+        height: 300,
+        padding: EdgeInsets.fromLTRB(10, 12, 10, 12),
+        decoration: BoxDecoration(
+          color: Color(0xFF3DDC84),
+          border: Border.all(color: Colors.red, width: 5, style: BorderStyle.solid),
+          borderRadius: BorderRadius.circular(100),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey,
+                offset: Offset(6, 6),
+                blurRadius: 10,
+                spreadRadius: 10),
+            BoxShadow(
+                color: Colors.blue.withOpacity(0.3),
+                offset: Offset(-6, -6),
+                blurRadius: 10,
+                spreadRadius: 10),
+          ]
         ),
+        child: Center(child: Container(
+            color: Colors.yellow.shade200,
+            child: Text("Hello Container Hello Container Hello Container Hello Container"))),
       ),
     );
   }
