3673696891e0b93843808a3aa6e106dbc3a3ff35
mimo626 <yessuk5499@gmail.com>
2024-05-15 20:29:27 +0900

    [feat]SingleChildScrollView 구현

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,12 @@ void main() {
       appBar: AppBar(
         title: Text("Study to Container"),
       ),
-      body: CustomContainer(),
+      body: Body(),
     ),
   ));
 }
 
+// 박스
 class CustomContainer extends StatelessWidget {
   const CustomContainer({super.key});
 
@@ -22,25 +23,117 @@ class CustomContainer extends StatelessWidget {
         height: 300,
         padding: EdgeInsets.fromLTRB(10, 12, 10, 12),
         decoration: BoxDecoration(
-          color: Color(0xFF3DDC84),
-          border: Border.all(color: Colors.red, width: 5, style: BorderStyle.solid),
-          borderRadius: BorderRadius.circular(100),
-          boxShadow: [
-            BoxShadow(
-                color: Colors.grey,
-                offset: Offset(6, 6),
-                blurRadius: 10,
-                spreadRadius: 10),
-            BoxShadow(
-                color: Colors.blue.withOpacity(0.3),
-                offset: Offset(-6, -6),
-                blurRadius: 10,
-                spreadRadius: 10),
-          ]
-        ),
-        child: Center(child: Container(
-            color: Colors.yellow.shade200,
-            child: Text("Hello Container Hello Container Hello Container Hello Container"))),
+            color: Color(0xFF3DDC84),
+            border: Border.all(
+                color: Colors.red, width: 5, style: BorderStyle.solid),
+            borderRadius: BorderRadius.circular(100),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(6, 6),
+                  blurRadius: 10,
+                  spreadRadius: 10),
+              BoxShadow(
+                  color: Colors.blue.withOpacity(0.3),
+                  offset: Offset(-6, -6),
+                  blurRadius: 10,
+                  spreadRadius: 10),
+            ]),
+        child: Center(
+            child: Container(
+                color: Colors.yellow.shade200,
+                child: Text(
+                    "Hello Container Hello Container Hello Container Hello Container"))),
+      ),
+    );
+  }
+}
+
+// 스크롤
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+          Container(
+            color: Colors.grey,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),
+        ],
       ),
     );
   }
