a5546cd53e46dd481a1e5faed2780b836ca069d4
mimo626 <yessuk5499@gmail.com>
2024-05-16 09:55:20 +0900

    [feat]Stateless, Stateful의 차이에 따른 index증가 구현

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
       appBar: AppBar(
         title: Text("Study to Container"),
       ),
-      body: HomeWidget(),
+      body: Body(),
     ),
   ));
 }
@@ -373,5 +373,87 @@ class ConstraintsWidget extends StatelessWidget {
     );
   }
 }
+
+// Stateless, Stateful의 차이
+class StateExample extends StatelessWidget {
+  const StateExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ExampleStateless(),
+        ExampleStateful(index: 3,),
+      ]
+    );
+  }
+}
+class ExampleStateless extends StatelessWidget {
+  const ExampleStateless({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
+class ExampleStateful extends StatefulWidget {
+  final int index;
+  const ExampleStateful({required this.index, super.key});
+
+  @override
+  State<ExampleStateful> createState() => _ExampleStatefulState();
+}
+class _ExampleStatefulState extends State<ExampleStateful> {
+  late int _index;
+  late TextEditingController textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    _index = widget.index;
+   textEditingController = TextEditingController();
+  }
+
+
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: GestureDetector(
+        onTap: (){
+          setState(() {
+            if(_index == 5){
+              _index = 0;
+              return;
+            }
+            _index++;
+          });
+        },
+        child: Container(
+          color: Colors.blue.withOpacity(_index/5),
+          child: Center(
+            child: Text('$_index'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
 
 
